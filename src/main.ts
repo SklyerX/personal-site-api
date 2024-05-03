a2256dfc4975367fd357d9c8ef367270e2bfd919
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2024-05-02 20:22:01 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   require('dotenv').config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();

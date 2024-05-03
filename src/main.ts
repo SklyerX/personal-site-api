e13a2166aca7f43969b91751c26151b2bf878817
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2024-05-02 20:40:52 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   require('dotenv').config();
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: "https://skylerx.ir",
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: "*"
+  });
   await app.listen(3000);
 }
 bootstrap();
